8a4cc98bee9019f3bb94829193e6c0b2ad5d3c16
notRachel <willowfairy@protonmail.com>
2022-09-18 08:32:42 +0800

    completed 1.0

---
+++ Utility/network.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+# Implement a web request interface to interact with Ricequant.
+
+from requests import Request, Session, exceptions
+import re, json
+
+from . import file
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'zh,zh-CN;q=0.7,en-US;q=0.3',
+        # 'Referer': rqConfig.rqEndpointUrl,
+        'X-Requested-With': 'XMLHttpRequest',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'TE': 'trailers'
+    }
+
+def prepareGenericRequest(urlRoute, urlParams, headers, method, sensitive=True):
+    
+    webRequest = Request(method, urlRoute, params=urlParams, headers=headers)
+    session = Session()
+    sess = session.prepare_request(webRequest)
+    
+    resp = session.send(sess) # 发报
+
+    return resp
+
---
+++ app.py
@@ -0,0 +1,82 @@
+#! /usr/bin/python3
+
+import json, re, os
+from bs4 import BeautifulSoup
+
+from Utility import file, network
+
+urlBase = "http://www.biqugse.com"
+
+def genericGet(webDoctUrl):
+    headers = network.headers
+    webDoctPld = network.prepareGenericRequest(webDoctUrl, {}, headers, "GET")
+
+    if(webDoctPld.status_code == 200):
+        webDoct = webDoctPld.text
+        
+        webDoctHtml = BeautifulSoup(webDoct, "html.parser")
+
+        return webDoctHtml
+    else:
+        return False
+
+def getNovelChapterList(novelId):
+    webDoctUrl = "{0}/{1}/".format(urlBase, novelId)
+    webDoctHtml = genericGet(webDoctUrl)
+    eligibleCells = webDoctHtml.find_all("dd")
+
+    links = []
+    for item in eligibleCells:
+        link = {}
+        chapterName = item.contents[0].text
+        chapterLink = item.contents[0].get("href")
+        link["chapterName"] = chapterName
+        link["chapterLink"] = chapterLink
+        links.append(link)
+
+    links = [dict(t) for t in set([tuple(d.items()) for d in links])]
+
+    return links
+
+def getNovelName(novelId):
+    webDoctUrl = "{0}/{1}/".format(urlBase, novelId)
+    webDoctHtml = genericGet(webDoctUrl)
+    if webDoctHtml is not False:
+        eligibleCells = webDoctHtml.select("#info > h1")
+        title = eligibleCells[0].text
+        return title
+    else:
+        return False
+
+def getSingleChapter(singleChapterUrl):
+    url = "{0}/{1}".format(urlBase, singleChapterUrl)
+    webDoctHtml = genericGet(url)
+    if webDoctHtml is not False:
+        eligibleCells = webDoctHtml.select("#content")
+        return eligibleCells[0].text
+    else:
+        return False
+
+def crawlAllChapters(novelId):
+    baseDir = "novels"
+    novelName = getNovelName(novelId)
+    print("Novel name: {0}".format(novelName))
+    os.mkdir(novelName)
+    allChapters = getNovelChapterList(novelId)
+    for item in allChapters:
+        print("Writing {0}".format(item["chapterName"]))
+        content = getSingleChapter(item["chapterLink"])
+        if content is not False:
+            content = re.sub(r' ', "\n", content)
+            content = re.sub(r'\t', "\n", content)
+            file.FileOperations.writeToFile(content, "{2}/{0}/{0} - {1}.txt".format(novelName, item["chapterName"], baseDir))
+
+if __name__ == '__main__':
+    userPrompt = input("Select novel you'd like to download: ")
+    novelName = getNovelName(userPrompt)
+    if (novelName is not False):
+        userConfirmation = input("Would you like to download {0}? (y/N)".format(novelName))
+        if (userConfirmation == "Y" or userConfirmation == "y"):
+            crawlAllChapters(userPrompt)
+    else:
+        print("Webpage not available")
